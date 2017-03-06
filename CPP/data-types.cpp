4dbd6cdeae28a81c7b36b015f7a0625e6fe281d3
Neha Sharma <neha.modgil@outlook.com>
2017-03-06 00:08:06 -0600

    basic data types

---
+++ CPP/data-types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a1;
+    long b1;
+    long long c1;
+    char d1;
+    float e1;
+    double f1;
+
+    scanf("%d %ld %lld %c %f %lf", &a1, &b1, &c1, &d1, &e1, &f1);
+    printf("%d \n%ld \n%lld \n%c \n%f \n%lf",a1, b1, c1, d1, e1, f1);
+
+    return 0;
+}
