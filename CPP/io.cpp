38ec87214ebffd9be4f7e7e17ae654043d295a23
Neha Sharma <neha.modgil@outlook.com>
2017-03-05 23:14:00 -0600

    solution for io problem

---
+++ CPP/io.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c, result;
+    scanf("%d %d %d", &a,&b,&c);
+    result = a+b+c;
+    printf("%d", result);
+    return 0;
+}
