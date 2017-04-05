636e8a170b622e25c351fb2b8b17a8332b760e7f
Neha Sharma <neha.modgil@outlook.com>
2017-04-04 23:10:11 -0500

    'Awk'-3

---
+++ LINUX_SHELL/GREP SED AWK/awk_3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+awk '{
+avg = ($2 + $3 + $4)/3;
+if(avg >= 80)
+    print $1,$2,$3,$4,": A";
+else  if(avg >= 60)
+    print $1,$2,$3,$4, ":", "B";
+else if(avg >= 50)
+      print $1,$2,$3,$4, ":", "C";
+else
+      print $1,$2,$3,$4, ":", "FAIL";
+
+}'
