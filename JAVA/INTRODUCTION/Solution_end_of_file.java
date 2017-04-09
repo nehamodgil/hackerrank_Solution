3a5332b7485ec5d76ba787896f96a508c3ab78d2
Neha Sharma <neha.modgil@outlook.com>
2017-04-09 14:20:38 -0500

    Java End-of-file

---
+++ JAVA/INTRODUCTION/Solution_end_of_file.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution_end_of_file {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    boolean flag = true;
+    int i = 0;
+    while(flag)
+    {
+      s=sc.nextLine();
+      if(s.length()==0)
+      {
+        flag = false;
+      }
+      else
+      {
+        i++;
+        System.out.println(i + " " + s);
+      }
+    }
+  }
+}
